dcdb741a16606bee5bc7943e5f873ea903a672ac
CreasyNet <zcg6556@163.com>
2024-03-15 13:18:09 +0800

    苍穹项目菜品分页查询功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,18 +1,23 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.impl.DishServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * 菜品管理
+ */
 @Slf4j
-@RequestMapping("/admin")
+@RequestMapping("/admin/dish")
 @RestController
+@Api(tags = "菜品相关接口")
 public class DishController {
 
     @Autowired
@@ -23,10 +28,25 @@ public class DishController {
      * @param dishDTO
      * @return
      */
-    @PostMapping("/dish")
+    @PostMapping
+    @ApiOperation("新增菜品")
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品：{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
         return Result.success();
+    }
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("菜品分页查询")
+    public Result<PageResult> pageQuery(DishPageQueryDTO dishPageQueryDTO){
+
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+
+        return Result.success(pageResult);
     }
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,8 +2,16 @@ package com.sky.service;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
 
+    /**
+     * 保存菜品
+     * @param dishDTO
+     */
     void saveWithFlavor(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
